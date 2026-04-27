143e9c737e19638297eca6a97dba8fea8da68d56
Matt Scholta <matthew.scholta@gmail.com>
2026-04-26 22:28:44 -0700

    chore: lets stem the bleading

--- applications/openthrottle-server/src/queues/plans/plans-queue.module.ts
+++ applications/openthrottle-server/src/queues/plans/plans-queue.module.ts
@@ -8,7 +8,10 @@ import { NestjsWorktreesModule } from '@openthrottle/nestjs-worktrees';
 import { MetricsModule } from '../../metrics/metrics.module';
 import { NotificationsModule } from '../../notifications/notifications.module';
 import { AgenticRalphModule } from '../agentic-ralph/agentic-ralph.module';
-import { PLANS_QUEUE_NAME } from './plans.constants';
+import {
+  PLANS_QUEUE_NAME,
+  RUN_PLAN_ORCHESTRATOR_JOB_NAME,
+} from './plans.constants';
 import { PlanRunCancellationService } from './plan-run-cancellation.service';
 import { PlansProcessor } from './plans.processor';
 
@@ -24,6 +27,8 @@ import { PlansProcessor } from './plans.processor';
     AgenticRalphModule,
     LoggerModule,
     MetricsModule,
+    NestjsBullmqModule.registerQueue(RUN_PLAN_ORCHESTRATOR_JOB_NAME),
+    NestjsBullmqBoardModule.forFeature(RUN_PLAN_ORCHESTRATOR_JOB_NAME),
     NestjsBullmqModule.registerQueue(PLANS_QUEUE_NAME),
     NestjsBullmqBoardModule.forFeature(PLANS_QUEUE_NAME),
     NestjsRepositoriesModule,
