5229c5cd46af3ebdfba63e041f4ff9d42085015e
Matt Scholta <matthew.scholta@gmail.com>
2026-06-05 23:43:42 -0700

    feat(react-router-profiling): scaffold profiling metrics components
    Generate ProfilingServerMetrics, ProfilingQueueMetrics, ProfilingTaskRunMetrics,
    and ProfilingQueueRunMetrics via @tools/generators:react batch invocation.
    
    Plan-Id: fdc7b263-e190-4ca2-a50a-308c84ba4561
    Task-Id: 7b0cd695-3d83-4630-a7eb-4f92a6c77ea4

---
+++ packages/react-router-profiling/src/components/ProfilingQueueRunMetrics.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+// import classnames from 'classnames';
+
+export interface ProfilingQueueRunMetricsProps {
+  // className?: string;
+}
+
+export const ProfilingQueueRunMetrics = (
+  _props: ProfilingQueueRunMetricsProps,
+): React.ReactElement => {
+  // const {  } = props;
+
+  // Hooks
+  const [_bool, _setBool] = React.useState(false);
+
+  // Setup
+
+  // Handlers
+
+  // Markup
+
+  // Life Cycle
+
+  // 🔌 Short Circuit
+
+  return (
+    <div className="p-4" data-testid="ProfilingQueueRunMetrics">
+      <h2>ProfilingQueueRunMetrics</h2>
+    </div>
+  );
+};
