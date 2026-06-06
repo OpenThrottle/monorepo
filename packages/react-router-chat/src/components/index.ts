85abd4daac8a4ee29b568fbc321de639ba9f9634
Matt Scholta <matthew.scholta@gmail.com>
2026-06-05 18:41:44 -0700

    chore: this and that

--- packages/react-router-chat/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { ChatComposer } from './ChatComposer';
-export type { ChatComposerProps } from './ChatComposer';
-export { ChatDialog } from './ChatDialog';
-export type { ChatDialogProps } from './ChatDialog';
-export { ChatMessageBody } from './ChatMessageBody';
-export type { ChatMessageBodyProps } from './ChatMessageBody';
-export { ChatThread } from './ChatThread';
-export type { ChatThreadProps } from './ChatThread';
