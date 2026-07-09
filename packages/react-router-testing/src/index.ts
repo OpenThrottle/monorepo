c8ff21137c57cb3fb8f35127c6879d10a49d6f00
Matt Scholta <matthew.scholta@gmail.com>
2026-07-08 17:53:39 -0700

    chore: adding a new package for testing

--- packages/react-router-testing/src/index.ts
+++ packages/react-router-testing/src/index.ts
@@ -1,4 +1,7 @@
-export * from './env';
-export * from './polyfills';
-export * from './router-context';
-export * from './setup';
+// export * from './components';
+// export * from './config';
+// export * from './data';
+// export * from './hooks';
+// export * from './utils';
+
+export const REMOVE_ME = `We can remove this when we actually have some code to export.`;
